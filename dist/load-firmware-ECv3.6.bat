43d246299d0ad39a06e969061ed2abb2f6549d85
Adam Mayer <phooky@gmail.com>
2012-03-01 14:47:37 -0500

    fixed baud and chip id for load firmware batch file

--- dist/load-firmware-ECv3.6.bat
+++ dist/load-firmware-ECv3.6.bat
@@ -8,13 +8,13 @@ set /p port= < serialPort
 
 set firmware=ECv3.6\EC-ecv34-v3.1_328.hex
 set programmer=stk500v1
-set baud=19200
-set part=m168
+set baud=57600
+set part=atmega328p
 
 
 echo Attempting to connect to port %port% 
 
-tools-win\avrdude -c%programmer% -b%baud% -D -v -V -F -p%part% -P%port% -Uflash:w:%firmware%:i 
+tools-win\avrdude2 -c%programmer% -b%baud% -D -v -V -F -p%part% -P%port% -Uflash:w:%firmware%:i 
 
 if errorlevel 1 (
 echo *** FAILURE *** Failed to verify program.  Try again.
